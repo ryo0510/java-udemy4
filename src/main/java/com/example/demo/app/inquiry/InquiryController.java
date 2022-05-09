a2dce1e0fb86d817011f021cd21050de8c8dd192
ryo0510 <3203ryo@gmail.com>
2022-05-09 21:03:33 +0900

    (finish)5-32_完了処理作成　～リダイレクトとリクエストスコープ～

--- src/main/java/com/example/demo/app/inquiry/InquiryController.java
+++ src/main/java/com/example/demo/app/inquiry/InquiryController.java
@@ -5,8 +5,10 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/inquiry")
@@ -14,7 +16,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class InquiryController {
 	
 	@GetMapping("/form")
-	public String form(InquiryForm inquiryForm, Model model) {
+	public String form(InquiryForm inquiryForm, 
+			Model model,
+			@ModelAttribute("complete") String complete) {
 		model.addAttribute("title", "Inquiry Form");
 		return "inquiry/form";
 	}
@@ -35,6 +39,19 @@ public class InquiryController {
 		}
 		model.addAttribute("title", "Confirm Page");
 		return "inquiry/confirm";
+	}
+	
+	@PostMapping("/complete")
+	public String complete(@Validated InquiryForm inquiryForm,
+			BindingResult result,
+			Model model,
+			RedirectAttributes redirectAttributes) {
+		if(result.hasErrors()) {
+			model.addAttribute("title", "InquiryForm");
+			return "inquiry/form";
+		}
+		redirectAttributes.addFlashAttribute("complete", "Registered");
+		return "redirect:/inquiry/form";
 	}
 
 }
