1b167360abd99728a41111c7c443d8eab72614c2
ryo0510 <3203ryo@gmail.com>
2022-05-14 15:26:07 +0900

    (add)6-40_DAOパターンを理解しましょう　～DAOクラスの作成～

---
+++ src/main/java/com/example/demo/dao/InquiryDaoImpl.java
@@ -0,0 +1,21 @@
+package com.example.demo.dao;
+
+import java.util.List;
+
+import com.example.demo.entity.Inquiry;
+
+public class InquiryDaoImpl implements InquiryDao {
+
+	@Override
+	public void insertInquiry(Inquiry inquiry) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public List<Inquiry> getAll() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}
