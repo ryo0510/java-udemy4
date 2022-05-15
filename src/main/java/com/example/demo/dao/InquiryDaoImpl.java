edd21d289cf3e1c069c9611f3875585bd91a3822
ryo0510 <3203ryo@gmail.com>
2022-05-15 22:35:24 +0900

    (finish)6-40_DAOパターンを理解しましょう　～DAOクラスの作成～

--- src/main/java/com/example/demo/dao/InquiryDaoImpl.java
+++ src/main/java/com/example/demo/dao/InquiryDaoImpl.java
@@ -1,21 +1,49 @@
 package com.example.demo.dao;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.Inquiry;
+@Repository
 
 public class InquiryDaoImpl implements InquiryDao {
+	
+	private final JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	public InquiryDaoImpl(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
 
 	@Override
 	public void insertInquiry(Inquiry inquiry) {
-		// TODO 自動生成されたメソッド・スタブ
+		jdbcTemplate.update("INSERT INTO inquiry(name, email, contents, created) VALUES(?, ?, ?, ?)",
+				inquiry.getName(), inquiry.getEmail(), inquiry.getContents(), inquiry.getCreated());
+				
 
 	}
 
 	@Override
 	public List<Inquiry> getAll() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		String sql = "SELECT id, name, email, contents, created FROM inquiry";
+		List<Map<String, Object>> resultlist = jdbcTemplate.queryForList(sql);
+		List<Inquiry> list = new ArrayList<Inquiry>();
+		for(Map<String, Object> result : resultlist) {
+			Inquiry inquiry = new Inquiry();
+			inquiry.setId((int)result.get("id"));
+			inquiry.setName((String)result.get("name"));
+			inquiry.setEmail((String)result.get("email"));
+			inquiry.setContents((String)result.get("contents"));
+			inquiry.setCreated(((Timestamp)result.get("created")).toLocalDateTime());
+			list.add(inquiry);
+		}
+		return list;
 	}
 
 }
